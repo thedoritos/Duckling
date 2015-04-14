8486ba507085c040336ae675f89310828634da80
thedoritos <thedoritos@gmail.com>
2015-04-14 22:08:39 +0900

    Create project

---
+++ Duckling/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Duckling
+//
+//  Created by t-matsumura on 4/14/15.
+//  Copyright (c) 2015 Humour Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
